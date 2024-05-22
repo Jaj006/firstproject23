13ebca40cd14d2f4538bcffefa8c7d7e527d36ac
Jaj006 <jasmohaprajnya@gmail.com>
2024-05-21 19:57:33 -0500

    Learn git2

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,17 @@
+package variousConcepts;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		
+	}
+public void printMe1() {
+	System.out.println("git test1");
+}
+public void printMe2() {
+	System.out.println("git test1");
+}
+public void printMe3() {
+	System.out.println("git test1");
+}
+}
